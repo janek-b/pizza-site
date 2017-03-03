c9ad7fe429f745bfc1af851578faa2614f104607
Janek Brandt <brandt.janek@gmail.com>
2017-03-03 10:03:20 -0800

    add section in html to display current order with pizza description and total price

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 // Back-End
-function Pizza(size, toppings) {
+function Pizza(size) {
   this.size = size;
-  this.toppings = toppings.slice();
+  this.toppings = [];
 };
 
 Pizza.prototype.getPrice = function() {
   // size small(10" $6) medium(12" $8) large(14" $10)
-  return (this.size - 4) + (this.toppings.length * 0.5);
+  return ((this.size - 4) + (this.toppings.length * 0.5)).toFixed(2);
+}
+
+Pizza.prototype.getDescription = function() {
+  var sizeDesc = "";
+  if (this.size === 10) {
+    sizeDesc = "Small"
+  } else if (this.size === 12) {
+    sizeDesc = "Medium"
+  } else if (this.size === 14) {
+    sizeDesc = "Large"
+  };
+  return sizeDesc + " Pizza with " + this.toppings.length + " toppings";
 }
 
 
@@ -21,12 +33,17 @@ $(function() {
   $("#pizzaForm").submit(function() {
     event.preventDefault();
     var inputSize = parseInt($("input:radio[name=pizzaSize]:checked").val());
-    var inputToppings = [];
+    var userPizza = new Pizza(inputSize);
+
     $("input:checkbox[name=topping]:checked").each(function() {
-      inputToppings.push($(this).val());
+      userPizza.toppings.push($(this).val());
     });
-    var userPizza = new Pizza(inputSize, inputToppings);
+
+    $("ul#pizzas").append("<li><span class'pizza'>" + userPizza.getDescription() + "</span></li>");
+
     console.log(userPizza);
     console.log(userPizza.getPrice());
+    console.log(userPizza.getDescription());
+    $("#orderPrice").text(userPizza.getPrice());
   })
 })
